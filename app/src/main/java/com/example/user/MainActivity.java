f01106d800956dbdd8c5aba83b49cf174d94b9ec
e <duanyudan93@163.com>
2018-04-27 09:24:23 +0800

    垂直显示的textview 仿古文排列

--- app/src/main/java/com/example/user/MainActivity.java
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
 VerticalTextView verticalTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verticalTextView.setmText("君子食无求饱，居无求安，敏于事而慎于言，就有道而正焉，可谓好学也已。");
